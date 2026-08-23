78c372dc2e98a41c856ff6c18f4108e36879ae53
On4ll <onallprobaho@gmail.com>
2026-08-23 17:45:44 +0600

    Added global scripts tempplate

---
+++ GlobalScripts/inventory_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func add_item(item_id: String, amount) -> bool:
+	return false
+	
+func remove_item(item_id: String, amount) -> bool:
+	return false
+	
+func does_have_item(item_id: String, amount) -> bool:
+	return false
