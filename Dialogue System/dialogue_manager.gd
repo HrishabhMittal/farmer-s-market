25fa2ccf24d10d0872d3245ecd2c12ff58727bd2
ST3zeta <stezan0811@gmail.com>
2026-08-24 09:53:46 +0200

    farmer bug fix and dialogue system upgrade

--- Dialogue System/dialogue_manager.gd
+++ Dialogue System/dialogue_manager.gd
@@ -1,4 +1,5 @@
 extends CanvasLayer
+class_name DialogueManager
 ##looks for lines to show and close when lines are finished
 
 @onready var label: Label = $Panel/Label
--- Dialogue System/dialogue_trigger_area.gd
+++ Dialogue System/dialogue_trigger_area.gd
@@ -3,13 +3,23 @@ class_name DialogueTriggerArea
 ##to enter the dialogue
 
 @export var dialogue_component: DialogueComponent
+@export var dialogue_manager: DialogueManager
 var player_in_range: bool = false
 
 func _ready() -> void:
-	body_entered.connect(func(body): if body.is_in_group("Player"): player_in_range = true)
-	body_exited.connect(func(body): if body.is_in_group("Player"): player_in_range = false)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if player_in_range and event.is_action_pressed("interact"):
 		if dialogue_component:
 			dialogue_component.start_dialogue()
+			dialogue_manager.show_dialogue(dialogue_component.dialogue_data)
