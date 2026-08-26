6e86c5c360bd0b37188f9745ef9b86c6a0dd89dd
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-08-26 19:57:33 +0530

    setup seed shop

--- Scenes/Seed shop/seed_bag.gd
+++ Scenes/Seed shop/seed_bag.gd
@@ -1,28 +1,34 @@
 extends Node2D
-var good
-var place_holder_common = [2,3,4]
-var place_holder_rare = [5,6]
-var chosen = 0
+var seed_type: int = 0
+var is_real: bool = true
+var fake_variant: int = 1
+var is_on_table: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if randf() >= 0.70:
-		chosen = place_holder_rare.pick_random()
-	else:
-		chosen = place_holder_common.pick_random() 
+@onready var spawn_point = $"spawn point"
+@onready var sprite = $"bag/place holder"
+@onready var click_area = $bag/seed_pack
+
+func _ready():
+	if not click_area.input_event.is_connected(_on_seed_pack_input_event):
+		click_area.input_event.connect(_on_seed_pack_input_event)
+
+func setup(type: int, real: bool, fake_var: int, minimised_texture: Texture2D):
+	seed_type = type
+	is_real = real
+	fake_variant = fake_var
+	
+	sprite.texture = minimised_texture
+	sprite.hframes = 5
+	sprite.vframes = 1
+	sprite.frame = seed_type
+
+func return_to_shelf():
+	is_on_table = false
+	global_position = spawn_point.global_position
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if chosen == 1:
-		$"bag/place holder".frame = 1
-	elif chosen == 2:
-		$"bag/place holder".frame = 2
-	elif chosen == 3:
-		$"bag/place holder".frame = 3
-	elif chosen == 4:
-		$"bag/place holder".frame = 4
-	elif chosen == 5:
-		$"bag/place holder".frame = 5
-	elif chosen == 6:
-		$"bag/place holder".frame = 6
+func _on_seed_pack_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var shop = get_tree().get_first_node_in_group("shop")
+		if shop:
+			shop.handle_bag_click(self)
---
+++ Scenes/Seed shop/seed_shop.gd
@@ -0,0 +1,71 @@
+extends Node2D
+
+@export var minimised_sheet: Texture2D
+@export var maximised_sheet: Texture2D
+
+@onready var shelf = $shelf
+@onready var table_marker = $Table
+@onready var action_menu = $CanvasLayer/ActionMenu
+@onready var inspect_overlay = $CanvasLayer/InspectOverlay
+@onready var inspect_sprite = $CanvasLayer/InspectOverlay/InspectSprite
+
+var bag_on_table = null
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _ready():
+	add_to_group("shop") 
+	refresh_shop()
+
+func refresh_shop():
+	for bag in shelf.get_children():
+		var type = randi() % 5
+		var is_real = randf() > 0.5
+		var fake_var = (randi() % 5) + 1 
+		
+		bag.setup(type, is_real, fake_var, minimised_sheet)
+		bag.return_to_shelf()
+		
+	bag_on_table = null
+	action_menu.hide()
+	inspect_overlay.hide()
+
+func handle_bag_click(bag):
+	if inspect_overlay.visible:
+		return
+		
+	if not bag.is_on_table:
+		if bag_on_table != null:
+			bag_on_table.return_to_shelf()
+		bag_on_table = bag
+		bag.is_on_table = true
+		bag.global_position = table_marker.global_position
+		action_menu.show()
+		var screen_pos = get_viewport().get_canvas_transform() * bag.global_position
+		action_menu.global_position = screen_pos + Vector2(60, -30)
+	else:
+		action_menu.visible = !action_menu.visible
+
+func _on_buy_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inspect_button_pressed() -> void:
+	action_menu.hide()
+	inspect_overlay.show()
+	
+	inspect_sprite.texture = maximised_sheet
+	inspect_sprite.hframes = 6
+	inspect_sprite.vframes = 5
+	
+	if bag_on_table.is_real:
+		inspect_sprite.frame = bag_on_table.seed_type * 6 
+	else:
+		inspect_sprite.frame = (bag_on_table.seed_type * 6) + bag_on_table.fake_variant
+
+
+func _on_inspect_overlay_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		inspect_overlay.hide()
