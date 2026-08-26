1d69854b5f6b81a4cc3e300383da67a22e403a25
Plazty <saiprakrut@gmail.com>
2026-08-26 22:16:19 +0530

    minimap fixes

--- Scripts/Minimap/building.gd
+++ Scripts/Minimap/building.gd
@@ -7,8 +7,17 @@ extends Area2D
 
 @export_file("*.tscn") var target_scene_path: String 
 @export var player_node: CharacterBody2D
+var target_pos = Vector2(0,0)
 
 const INTERACTION_DISTANCE = 5.0
+
+func _process(delta: float) -> void:
+	if global_position == Vector2(-401.0,98.0):
+		target_pos = Vector2(-278,137)
+	elif global_position == Vector2(97,-222):
+		target_pos = Vector2(-15,-164)
+	elif global_position == Vector2(390,-55):
+		target_pos = Vector2(267,-29)
 
 @warning_ignore("unused_parameter")
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -19,4 +28,4 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 				if distance<INTERACTION_DISTANCE:
 					TravelTransition.change_scene(target_scene_path)
 				else:	
-					player_node.travel_to_building(global_position)
+					player_node.travel_to_building(target_pos)
