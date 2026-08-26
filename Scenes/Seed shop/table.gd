76e9ab0ab7b96592a525ae7e20dc1a1329ff7c8a
Plazty <saiprakrut@gmail.com>
2026-08-26 18:01:20 +0530

    random seed packs

--- Scenes/Seed shop/table.gd
+++ Scenes/Seed shop/table.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
+@onready var seed_bag = $"."
+ 
+
+
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "seed_pack":
 		area.get_parent().scale.x = 3
 		area.get_parent().scale.y = 3
-
+		area.on_table = true
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.name == "seed_pack":
 		area.get_parent().scale.x = 1
 		area.get_parent().scale.y = 1
+		area.on_table = false
