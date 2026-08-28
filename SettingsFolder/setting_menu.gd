ce5b8dcf2eede1dde9cfdb701de8c3f62cc56f46
ST3zeta <stezan0811@gmail.com>
2026-08-28 19:50:36 +0200

    fulls creen toggle remove and title screen button

--- SettingsFolder/setting_menu.gd
+++ SettingsFolder/setting_menu.gd
@@ -7,9 +7,13 @@ var is_open: bool = false
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("esc"):
 		is_open = !is_open
-		update_panel_visiblity(is_open)
+		update_panel(is_open)
 
-func update_panel_visiblity(direction: bool) -> void:
+func update_panel(direction: bool) -> void:
 	match direction:
 		true: anim_player.play("open")
 		false: anim_player.play("close")
+
+
+func _on_screen_title_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu/main_menu.tscn")
