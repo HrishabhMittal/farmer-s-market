2b4d8ad1069bf44556a37a4a2ba69f05ba2ea0c4
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-08-28 14:35:38 +0530

    rework some farming mechanics

--- CoreFarmSystem/farm_plants/farm_plant.gd
+++ CoreFarmSystem/farm_plants/farm_plant.gd
@@ -6,6 +6,7 @@ var tile_manager: FarmTileManager
 @export var plant_texture: TextureRect
 @export var produced_crop_id: String
 @export var production_amount: int = 1
+
 var growth_cycle_day: Array[int] = [1, 2]
 @export var growth_cycle_texture: Array[Texture2D]= [null, null, null]
 @export var growth_chance: int = 100
@@ -13,9 +14,11 @@ var growth_cycle_day: Array[int] = [1, 2]
 var current_growth_time: int = 0
 var current_growth_cycle: int = 0
 var is_fully_grown: bool = false
+var is_hovered: bool = false
 
 func _ready():
 	SignalBus.global_growth_tick.connect(_on_growth_tick)
+	
 	plant_texture.gui_input.connect(_handle_harvest_attempt)
 	plant_texture.mouse_entered.connect(highlight)
 	plant_texture.mouse_exited.connect(unhighlight)
@@ -23,10 +26,16 @@ func _ready():
 	if not is_fully_grown and growth_cycle_texture.size() > current_growth_cycle:
 		plant_texture.texture = growth_cycle_texture[current_growth_cycle]
 
+func _process(_delta: float) -> void:
+	if is_hovered and is_fully_grown:
+		# If the player is holding the button down while hovering, attempt to harvest
+		if Input.is_action_pressed("left click") or Input.is_action_pressed("interact"):
+			harvest()
+
 func _on_growth_tick() -> void:
 	if is_fully_grown:
 		return
-		
+	
 	var should_grow: bool = randi_range(1, 100) <= growth_chance
 	if not should_grow:
 		return
@@ -35,15 +44,14 @@ func _on_growth_tick() -> void:
 		production_amount += 1
 		InfocardManager.show_floating_text("+1", global_position, "Green")
 		tile_manager.unwater_tile(global_position)
+		current_growth_time += 1
+		_handle_growth()
 		
-	current_growth_time += 1
-	_handle_growth()
-
 func process_missed_ticks(ticks: int) -> void:
 	for i in range(ticks):
 		if is_fully_grown:
 			break
-			
+		
 		var should_grow: bool = randi_range(1, 100) <= growth_chance
 		if not should_grow:
 			continue
@@ -51,9 +59,8 @@ func process_missed_ticks(ticks: int) -> void:
 		if is_watered():
 			production_amount += 1
 			tile_manager.unwater_tile(global_position)
-			
-		current_growth_time += 1
-		_handle_growth()
+			current_growth_time += 1
+			_handle_growth()
 
 func _handle_growth() -> void:
 	if current_growth_time >= growth_cycle_day[current_growth_cycle]:
@@ -64,30 +71,40 @@ func _handle_growth() -> void:
 	if current_growth_cycle >= growth_cycle_day.size():
 		is_fully_grown = true
 
+
 func _handle_harvest_attempt(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") or event.is_action_pressed("left click"):
 		if is_fully_grown:
 			harvest()
-			get_viewport().set_input_as_handled()
-
+		get_viewport().set_input_as_handled()
+		
 func harvest() -> void:
-	## Method 1: Add item directly to players inventory, But that will fail if player has full inventory
-	#InventoryManager.add_item_to_barn(produced_crop_id, production_amount)
-	
-	## Method 2: Drop harvested crop on the ground. He can pick it up later if inventory is full so item is not lost
+	var player = get_tree().get_first_node_in_group("Player")
+	if player:
+		var interact_area = player.get_node_or_null("InteractRadius")
+		if interact_area:
+			var col_shape = interact_area.get_node_or_null("CollisionShape2D")
+			if col_shape:
+				var max_distance = col_shape.shape.radius
+				if player.global_position.distance_to(global_position) > max_distance:
+					return
 	ItemManager.spawn_ground_item_from_id(produced_crop_id, production_amount, global_position)
-	
 	SignalBus.crop_harvested.emit(self)
 	tile_manager.untill_tile(global_position)
+	InfocardManager.hide_farmplant_infocard() 
 	call_deferred("queue_free")
 
 func is_watered() -> bool:
 	return tile_manager.get_tile_type(global_position) == "watered"
 
+
 func highlight() -> void:
 	modulate = Color(1.2, 1.2, 1.2, 1)
 	InfocardManager.show_farmplant_infocard(self)
+	is_hovered = true
+
 
 func unhighlight() -> void:
 	modulate = Color(1.0, 1.0, 1.0, 1)
 	InfocardManager.hide_farmplant_infocard()
+	is_hovered = false
--- CoreFarmSystem/farm_tool_manager.gd
+++ CoreFarmSystem/farm_tool_manager.gd
@@ -3,65 +3,74 @@ class_name FarmToolManager
 
 signal tool_selected(new_tool: FarmTool)
 
-# All the tools are self explanatory
-# The SEED_PLANTER will be selected when player has a seed picked up on the cursor
 enum FarmTools {SHOVEL, HOE, WATERING_CAN, SEED_PLANTER, NONE}
-var selected_tool: FarmTools = FarmTools.WATERING_CAN
 
+var selected_tool: FarmTools = FarmTools.WATERING_CAN
 var tile_manager: FarmTileManager
 
-# Parent node will call it to initialize
+var is_using_tool: bool = false
+
 func initialize(new_tile_manager: FarmTileManager) -> void:
 	tile_manager = new_tile_manager
 	tool_selected.connect(handle_new_tool_selection)
-	
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("right click"):
 		selected_tool = FarmTools.NONE
-	
+		is_using_tool = false
+
 	if event.is_action_pressed("use_farm_tool"):
-		var mouse_pos = get_global_mouse_position()
-		var player = get_tree().get_first_node_in_group("Player")
-		if player:
-			var interact_area = player.get_node_or_null("InteractRadius")
-			if interact_area:
-				var col_shape = interact_area.get_node_or_null("CollisionShape2D")
-				var max_distance = col_shape.shape.radius
-				var tile_center = tile_manager.get_tile_center(mouse_pos)
-				
-				if player.global_position.distance_to(tile_center) > max_distance:
-					print("too far 4 tool")
-					return
-					
-				match selected_tool:
-					FarmTools.SHOVEL:
-						if tile_manager.get_tile_type(mouse_pos) == "grass":
-							dig_ground()
-					FarmTools.HOE:
-						if tile_manager.get_tile_type(mouse_pos) == "dirt":
-							till_ground()
-					FarmTools.WATERING_CAN:
-						if tile_manager.get_tile_type(mouse_pos) == "tilled":
-							water_ground()
-					FarmTools.SEED_PLANTER:
-						var tile_under_mouse: String = tile_manager.get_tile_type(mouse_pos)
-						if not tile_manager.is_crop_planted(mouse_pos):
-							if tile_under_mouse == "tilled" or tile_under_mouse == "watered":
-								# Check the new dedicated seed slot instead of PlayerHeldItem
-								var active_seed = StateManager.active_seed_inventory.slots[0]
-								if active_seed and ItemTypes.ItemType.SEED in active_seed.item_data.item_type:
-									plant_seed()
+		is_using_tool = true
+		_apply_tool()
+	elif event.is_action_released("use_farm_tool"):
+		is_using_tool = false
+
+
+func _process(_delta: float) -> void:
+	if is_using_tool:
+		_apply_tool()
+
+
+func _apply_tool() -> void:
+	var mouse_pos = get_global_mouse_position()
+	var player = get_tree().get_first_node_in_group("Player")
+	
+	if player:
+		var interact_area = player.get_node_or_null("InteractRadius")
+		if interact_area:
+			var col_shape = interact_area.get_node_or_null("CollisionShape2D")
+			var max_distance = col_shape.shape.radius
+			var tile_center = tile_manager.get_tile_center(mouse_pos)
+			
+			if player.global_position.distance_to(tile_center) > max_distance:
+				return
+
+	match selected_tool:
+		FarmTools.SHOVEL:
+			if tile_manager.get_tile_type(mouse_pos) == "grass":
+				dig_ground()
+		FarmTools.HOE:
+			if tile_manager.get_tile_type(mouse_pos) == "dirt":
+				till_ground()
+		FarmTools.WATERING_CAN:
+			if tile_manager.get_tile_type(mouse_pos) == "tilled":
+				water_ground()
+		FarmTools.SEED_PLANTER:
+			var tile_under_mouse: String = tile_manager.get_tile_type(mouse_pos)
+			if not tile_manager.is_crop_planted(mouse_pos):
+				if tile_under_mouse == "tilled" or tile_under_mouse == "watered":
+					var active_seed = StateManager.active_seed_inventory.slots[0]
+					if active_seed and ItemTypes.ItemType.SEED in active_seed.item_data.item_type:
+						plant_seed()
 
 func plant_seed() -> void:
 	var seed_item: Item = StateManager.active_seed_inventory.slots[0]
-
 	if seed_item:
 		var new_plant: FarmPlant = seed_item.item_data.scene_to_instantiate.instantiate()
 		add_child(new_plant)
 		new_plant.tile_manager = tile_manager
 		new_plant.global_position = tile_manager.get_tile_center(get_global_mouse_position())
 		tile_manager.crop_planted(get_global_mouse_position())
-		
 		StateManager.active_seed_inventory.remove_item(seed_item, 1)
 	else:
 		prints("failed to plant")
@@ -71,9 +80,9 @@ func handle_new_tool_selection(new_tool: FarmTool) -> void:
 
 func dig_ground() -> void:
 	tile_manager.dig_ground()
-	
+
 func till_ground() -> void:
 	tile_manager.till_ground()
-	
+
 func water_ground() -> void:
 	tile_manager.water_ground()
