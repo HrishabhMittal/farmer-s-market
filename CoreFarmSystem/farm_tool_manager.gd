2afec88d081efff82dd1c024b9ac4b372e32fbdc
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-08-30 06:52:54 +0530

    minor bug fixes in farm, update dialog for exit pop up

--- CoreFarmSystem/farm_tool_manager.gd
+++ CoreFarmSystem/farm_tool_manager.gd
@@ -77,7 +77,7 @@ func plant_seed() -> void:
 	var seed_item: Item = StateManager.active_seed_inventory.slots[0]
 	if seed_item:
 		var new_plant: FarmPlant = seed_item.item_data.scene_to_instantiate.instantiate()
-		add_child(new_plant)
+		get_parent().add_child(new_plant)
 		new_plant.generate_display_name(seed_item)
 		new_plant.production_amount = GameConfig.crop_seed_yields.get(seed_item.item_data.item_id, 1) # Tries to get the seeds production amount from config , if fails to find id, sets it to 1
 		new_plant.tile_manager = tile_manager
